3898fd2bb911247e1140957ad7418ee1287f36a0
Luke Gill <bagrad007@gmail.com>
2021-03-29 10:36:27 -0400

    adds forms for routes login, signup. Adds instance variable for new action in sessions controller. Renames session controller to proper plurilized sessions controller to match routes. Changes session view to sessions view for proper convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
