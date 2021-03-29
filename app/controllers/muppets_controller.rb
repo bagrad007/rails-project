b01a9bf3407fcf968d8bd49412f0dc30891c3e56
Luke Gill <bagrad007@gmail.com>
2021-03-29 10:58:55 -0400

    adds nested form for new muppets

--- app/controllers/muppets_controller.rb
+++ app/controllers/muppets_controller.rb
@@ -1,2 +1,12 @@
 class MuppetsController < ApplicationController
+  def new
+    @muppet = Muppet.new
+  end
+
+  def create
+  end
+
+  def muppet_params
+    params.require(:muppet).permit(:name, :job)
+  end
 end
